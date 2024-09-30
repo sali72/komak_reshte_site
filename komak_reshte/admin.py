7579d122a82c8b1e33500006bf982512b492cb34
Ali <sahashemi072@gmail.com>
2024-09-30 12:07:49 +0330

    Fix app not running
    admin panel had issues

--- komak_reshte/admin.py
+++ komak_reshte/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin,  StackedInline, TabularInline  # Import ModelAdmin and Inline classes
 
 from .models import University, FieldOfStudy, EnrollmentData
 
+class EnrollmentDataInline(TabularInline):
+    model = EnrollmentData
+    fk_name = 'field_of_study'
 
-admin.site.register(University, FieldOfStudy)
+class FieldOfStudyAdmin(ModelAdmin):
+    inlines = [EnrollmentDataInline]
+
+admin.site.register(University)
+admin.site.register(FieldOfStudy, FieldOfStudyAdmin)
